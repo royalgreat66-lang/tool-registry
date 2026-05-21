c4269ddc8ecebe1c530839e674d2d3a88c881f23
royalgreat66-lang <royalgreat66@gmail.com>
2026-05-21 20:50:28 +0300

    jason export name

--- src/App.jsx
+++ src/App.jsx
@@ -201,7 +201,7 @@ function AppContent() {
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = url;
-        a.download = `tool-registry-${new Date().toISOString().split('T')[0]}.json`;
+        a.download = `link-house-${new Date().toISOString().split('T')[0]}.json`;
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
